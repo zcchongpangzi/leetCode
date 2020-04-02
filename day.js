377b8e3e8ca3dd781024d4984c5690f7f83ed22c
zcchongpangzi <cjp030369@163.com>
2020-04-02 10:36:27 +0800

    生命游戏

--- day.js
+++ day.js
@@ -676,3 +676,104 @@ var restoreIpAddresses = function(s) {
   
   return res;
 };
+
+// 289. 生命游戏
+// 根据 百度百科 ，生命游戏，简称为生命，是英国数学家约翰·何顿·康威在 1970 年发明的细胞自动机。
+
+// 给定一个包含 m × n 个格子的面板，每一个格子都可以看成是一个细胞。每个细胞都具有一个初始状态：1 即为活细胞（live），或 0 即为死细胞（dead）。每个细胞与其八个相邻位置（水平，垂直，对角线）的细胞都遵循以下四条生存定律：
+
+// 如果活细胞周围八个位置的活细胞数少于两个，则该位置活细胞死亡；
+// 如果活细胞周围八个位置有两个或三个活细胞，则该位置活细胞仍然存活；
+// 如果活细胞周围八个位置有超过三个活细胞，则该位置活细胞死亡；
+// 如果死细胞周围正好有三个活细胞，则该位置死细胞复活；
+// 根据当前状态，写一个函数来计算面板上所有细胞的下一个（一次更新后的）状态。下一个状态是通过将上述规则同时应用于当前状态下的每个细胞所形成的，其中细胞的出生和死亡是同时发生的。
+
+// 感觉这个题没意思啊，就是看你思路请不清晰。。。标记后，循环修改就完事了。。
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function(board) {
+
+ function checkStatue (i,j,board,val) {
+
+     let count = 0;
+     if(i - 1 >= 0) {
+          if (j - 1 >= 0) {
+             if (board[i - 1][j - 1] == 1 || board[i - 1][j - 1] == 3 ){
+                  count ++;
+             }
+          }
+          if (board[i - 1][j] == 1 || board[i - 1][j] == 3 ){
+                  count ++;
+          }
+          if (j + 1 < board[i - 1].length) {
+             if (board[i - 1][j + 1] == 1 || board[i - 1][j + 1] == 3 ){
+                  count ++;
+             }
+          }
+     }
+     if (j - 1 >= 0) {
+          if (board[i][j - 1] == 1 || board[i][j - 1] == 3 ){
+              count ++;
+          }
+      }
+  
+      if (j + 1 < board[i].length) {
+          if (board[i][j + 1] == 1 || board[i][j + 1] == 3 ){
+              count ++;
+          }
+      }
+      if(i + 1 < board.length) {
+          if (j - 1 >= 0) {
+             if (board[i + 1][j - 1] == 1 || board[i + 1][j - 1] == 3 ){
+                  count ++;
+             }
+          }
+          if (board[i + 1][j] == 1 || board[i + 1][j] == 3 ){
+                  count ++;
+          }
+          if (j + 1 < board[i + 1].length) {
+             if (board[i + 1][j + 1] == 1 || board[i + 1][j + 1] == 3 ){
+                  count ++;
+             }
+          }
+     }
+
+      
+      if (val == 0) {
+          if(count == 3) {
+              return 2;
+          } else {
+              return 0;
+          }
+      }
+      if (val == 1) {
+          if(count < 2 || count > 3) {
+              return 3;
+          } else {
+              return 1;
+          }
+      }
+
+ }
+
+ for(let i = 0; i < board.length; i ++) {
+     let tempArr = board[i];
+     for(let j = 0; j < tempArr.length; j++) {
+          board[i][j] = checkStatue(i,j,board,board[i][j]);
+     }
+ }
+ for(let i = 0; i < board.length; i ++) {
+     let tempArr = board[i];
+     for(let j = 0; j < tempArr.length; j++) {
+         if(board[i][j] == 2){
+              board[i][j] = 1;
+         };
+         if(board[i][j] == 3){
+              board[i][j] = 0;
+         };
+     }
+ }
+
+};
